5729febaa7e46bd1dfef61a0452fe4a9d33b8ecd
Xzoba54 <83954330+Xzoba54@users.noreply.github.com>
2022-08-23 22:36:41 +0200

    Add files via upload

---
+++ api/DataBreakTime.js
@@ -0,0 +1,12 @@
+[
+  null,
+  hour == 8 && minute > 30 && minute < 35,
+  hour == 9 && minute > 20 && minute < 25,
+  hour == 10 && minute > 10 && minute < 20,
+  hour == 11 && minute > 5 && minute < 10,
+  (hour == 11 && minute > 55) || (hour == 12 && minute < 15),
+  hour == 13 && minute > 0 && minute < 5,
+  hour == 13 && minute > 50 && minute < 55,
+  hour == 14 && minute > 40 && minute < 45,
+  hour == 15 && minute > 30 && minute < 35,
+];
---
+++ api/DataTime.js
@@ -0,0 +1,13 @@
+[
+  null,
+  (hour == 7 && minute >= 45) || (hour == 8 && minute <= 30),
+  (hour == 8 && minute >= 35) || (hour == 9 && minute <= 20),
+  (hour == 9 && minute >= 25) || (hour == 10 && minute <= 10),
+  (hour == 10 && minute >= 20) || (hour == 11 && minute <= 5),
+  hour == 11 && minute >= 10 && minute <= 55,
+  (hour == 12 && minute >= 15) || (hour == 13 && minute == 0),
+  hour == 13 && minute >= 5 && minute <= 50,
+  (hour == 13 && minute >= 55) || (hour == 14 && minute <= 40),
+  (hour == 14 && minute >= 45) || (hour == 15 && minute <= 30),
+  (hour == 15 && minute >= 35) || (hour == 16 && minute <= 20),
+];
